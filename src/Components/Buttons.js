9458d97e9eac1f26dd9a67eedc235fd248c3b2b6
sarahyjja <sarahkharraz@gmail.com>
2019-02-26 16:22:43 +0000

    test check if we have a decrement button

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Buttons = (props) => {
     return ( 
         <div>
-            <button className="ui red basic button" onClick={props.onDecrement}>Decrement</button>
+            <button className="ui red basic button" onClick={props.onDecrement} test='decrementButton'>Decrement</button>
             <button className="ui green basic button"onClick={props.onIncrement}>Increment</button>
         </div>
      );
---
+++ src/Components/__tests__/Buttons.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import Buttons from '../Buttons';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without crashing", () => {
+    shallow(<Buttons />);
+});
+
+test("check if we have a decrement button", () => {
+    const wrapper = shallow(<Buttons />);
+    const decrementButton = wrapper.find(`[test='decrementButton']`)
+
+    expect(decrementButton.hasClass('red')).toEqual(true)
+})
